78dd424142751c1167343a50ab73e656ef25fab9
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2024-02-13 09:45:08 -0500

    current rp mirrot

---
+++ src/rp-mirror/init-ahe.sh
@@ -0,0 +1,2 @@
+bash /opt/redpitaya/sbin/overlay.sh v0.94
+python /opt/data/hasith/main.py
---
+++ src/rp-mirror/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import time
+import rp
+
+period = 1e-2      # period in secodns
+
+# Initialize the interface
+rp.rp_Init()
+
+#####! Choose one of two methods, comment the other !#####
+#! METHOD 1: Interacting with Registers direclty
+while 1:
+
+    for i in range(0,255):
+        rp.rp_LEDSetState(i)     # or 0b00000001
+        time.sleep(period/2.0)
+        rp.rp_LEDSetState(0b00000000)     # or 0
+
+# Release resources
+rp.rp_Release()
+
